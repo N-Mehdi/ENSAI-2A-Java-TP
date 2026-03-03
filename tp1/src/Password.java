c1382f17fa5d13cd368ab9b737ae7dd801b62a26
N-Mehdi <mehdineji14@gmail.com>
2026-03-03 11:02:58 +0000

    bruteforce

--- tp1/src/Password.java
+++ tp1/src/Password.java
@@ -39,9 +39,12 @@ public class Password {
      * @return the 6-digit number that matches, or null if no match is found
      */
     public static String bruteForce6Digit(String targetHash) {
-
-        // Code here
-
+        for (int i = 0; i < 999999; i++) {
+            String testpassword = hashPassword(String.format("%06d", i));
+            if (targetHash.equals(testpassword)) {
+                return String.format("%06d", i);
+            }
+        }
         return null;
     }
 
